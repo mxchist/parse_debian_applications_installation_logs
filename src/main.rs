b0118aebc7919ec0346cf765df7757f2a274e959
Chistyakov Maksim <gods.like.you@gmail.com>
2025-12-30 12:19:49 +0400

    Работающая версия, которая выдаёт список всех пакетов после установки

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use std::fs;
 use regex::Regex;
-use std::str::FromStr;
+//use std::str::FromStr;
 use std::vec::Vec;
 use std::fmt;
+//use pretty_assertions::{assert_eq, Comparison};
 
 #[derive(Debug)]
 enum Action {
     Installed,
     Remove,
-    Other,
+    StartupPackagesRemove,
+    Other(CouldntParse),
 }
 
 #[derive(Debug)]
@@ -16,14 +18,13 @@ struct CouldntParse {
     line: String,
 }
 
-impl FromStr for Action {
-    type Err = CouldntParse;
+impl Action {
 
-    fn from_str(s: &str) -> Result<Self, CouldntParse> {
+    fn from_str(s: &str) -> Self {
         match s {
-            "installed" => Ok(Action::Installed),
-            "remove" => Ok(Action::Remove),
-            _ => Err(CouldntParse{line: String::from(s)}),
+            "installed" => Action::Installed,
+            "remove" => Action::Remove,
+            _ => Action::Other(CouldntParse{line: String::from(s)}),
         }   
     }
     
@@ -46,34 +47,64 @@ fn main() {
 
     let contents = fs::read_to_string(String::from("/home/max/Documents/system_config/var/log/dpkg.log")).unwrap();
     let mut lines = contents.lines();
-    let mut last_line: &str;
-    for _i in 1..1000 {
-        last_line = lines.next_back().unwrap();
-        let event = get_event(last_line);
+    println!("The initial lines count is: {}", lines.clone().count());
+    while let Some(last_line) = lines.next_back() {
+        let event = get_event(last_line, &lines.clone().count());
         match event.action {
-            Action::Installed => to_install.push(event.package_name),
+            Action::Installed => {
+                match to_remove.iter().position(|key| key.eq(&event.package_name)) {
+                    Some(p) => {
+                        //let to_remove_before = to_remove.clone();
+                        to_remove.remove(p);
+                        //assert_eq!(to_remove_before, to_remove, "the last_line: {}");
+                        //println!("the package to remove: {}, the to_remove:\n{}", &event.package_name, Comparison::new(&to_remove_before, &to_remove));
+                    },
+                    None => to_install.push(event.package_name),
+                }
+            },
             Action::Remove => to_remove.push(event.package_name),
-            _ => (),
+            Action::Other(couldnt_parse) => eprintln!("not parsed line: {couldnt_parse}"),
+            Action::StartupPackagesRemove => (),
         };
-        dbg!("{}", get_event(last_line));
     }
+    dbg!(":?", &to_remove);
+    dbg!(":?", &to_install);
 }
 
-fn get_event<'a>(last_line: &'a str) -> InstallationStatus {
-    let regex = Regex::new(r"^2\d{3}\-\d\d\-\d\d \d\d:\d\d:\d\d (status)? (?<action>installed|remove) (?<package_name>[^:]+):").unwrap();
-    let caps = regex.captures(last_line).unwrap();
+fn get_event<'a>(last_line: &'a str, lines_count: &usize) -> InstallationStatus {
+    let re = Regex::new(r"^2\d{3}\-\d\d\-\d\d \d\d:\d\d:\d\d (status )?(?<action>installed|remove) (?<package_name>[^:]+):").unwrap();
+    let caps = re.captures(last_line);
     let mut installation_status = InstallationStatus{
-        action: Action::Other,
+        action: Action::Other(CouldntParse{line: String::from("init")}),
         package_name: String::new(),
     };
-    match caps.name("action") {
-        Some(action) => {
-            installation_status.action = Action::from_str(action.as_str()).inspect_err(|e| eprintln!("{e}")).unwrap_or(Action::Other);
-            installation_status.package_name = caps.name("package_name").unwrap().as_str().to_string();
+    match caps {
+        Some(caps) => match caps.name("action") {
+            Some(action) => {
+                installation_status.action = Action::from_str(action.as_str());
+                installation_status.package_name = caps.name("package_name").unwrap().as_str().to_string();
+            },
+            None => eprintln!("Caption is empty. last_line is: {last_line}"),
         },
-        None => eprintln!("{last_line}"),
+        None => installation_status.action = check_startup_packages_remove(last_line).or_else(
+            || {eprintln!("the lines count is: {lines_count}, the last_line is: {last_line}");
+            None}
+        ).unwrap(),
     }
-    caps.name("action").unwrap().as_str().to_string();
     installation_status
 }
 
+fn check_startup_packages_remove<'a>(last_line: &'a str) -> Option<Action> {
+    let re = Regex::new(r"^2\d{3}\-\d\d\-\d\d \d\d:\d\d:\d\d (?<startup_packages>startup packages remove)").unwrap();
+    match re.captures(last_line) {
+        Some(caps) => {
+            if let Some(_) = caps.name("startup_packages")  {
+                Some(Action::StartupPackagesRemove)
+            }
+            else {
+                None
+            }
+        },
+        None => None,
+    }
+}
